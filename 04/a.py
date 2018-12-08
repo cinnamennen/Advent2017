388eff4fab01362abcea9f84e1a2ac1ee423d985
Caleb Mennen <git@calebmennen.com>
2018-12-08 16:34:07 -0500

    4a

--- 04/a.py
+++ 04/a.py
@@ -1,5 +1,9 @@
-file = 'x.txt'
+import pprint
+
+pp = pprint.PrettyPrinter(indent=4)
+file = 'a.txt'
 data = [_.strip() for _ in open(file).readlines()]
-
-
-print(data)
+data = [d.split() for d in data]
+data = [(sorted(list(set(d))) == sorted(d)) for d in data]
+data = data.count(True)
+pp.pprint(data)
