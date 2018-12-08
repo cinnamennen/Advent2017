d9309b90b18daab148582b17b641b5315fc4945f
Caleb Mennen <git@calebmennen.com>
2018-12-08 15:13:36 -0500

    Initial commit

---
+++ 01/a.py
@@ -0,0 +1,14 @@
+file = 'a.txt'
+data = [_.strip() for _ in open(file).readlines()]
+
+
+def clean(input: str):
+    output = []
+    for index in range(len(input)):
+        if input[index] == input[index-1]:
+            output.append(int(input[index]))
+
+    return sum(output)
+
+
+print([clean(x) for x in data])
