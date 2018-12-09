bfe055a33dc23ebaae700d1dc0a8ebcc7db822ae
Caleb Mennen <git@calebmennen.com>
2018-12-09 13:39:22 -0500

    8 approach 1

--- 08/a.py
+++ 08/a.py
@@ -1,6 +1,45 @@
 import pprint
+import re
+from collections import defaultdict
 
 pp = pprint.PrettyPrinter(indent=4)
 
-file = 'x.txt'
+file = 'a.txt'
 data = [_.strip() for _ in open(file).readlines()]
+data = [re.search(r"(\w) (\w*) (-?\d*) if (\w*) (.*) (-?\d*)", d).groups() for d in data]
+
+registers = defaultdict(lambda: 0)
+
+
+def test(a, op, b):
+    if op == '==':
+        return a == b
+    if op == '!=':
+        return a != b
+    if op == '>':
+        return a > b
+    if op == '<':
+        return a < b
+    if op == '<=':
+        return a <= b
+    if op == '>=':
+        return a >= b
+    raise ValueError
+
+
+def calculate(a, op, b):
+    if op == 'inc':
+        return a + b
+    if op == 'dec':
+        return a - b
+
+
+for register, op, amount, check, comparison, value in data:
+    print(f"{check} {comparison} {value} is {registers[check]} {comparison} {int(value)} which is ", end="")
+    if test(registers[check], comparison, int(value)):
+        print("true")
+        registers[register] = calculate(registers[register], op, int(amount))
+    else:
+        print("false")
+print(max(registers.values()))
+pp.pprint(registers)
