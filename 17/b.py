bbfd3e60b8504fc7eca75ed4b8e2c479a3dd659a
Caleb Mennen <git@calebmennen.com>
2018-12-14 14:09:41 -0500

    17

--- 17/b.py
+++ 17/b.py
@@ -10,7 +10,7 @@ pp = pprint.PrettyPrinter(indent=4)
 data = 343
 
 d = deque([0])
-for i in trange(1,50000001, mininterval=1, miniters=100):
+for i in trange(1, 50000001, mininterval=1, miniters=100):
     d.rotate(-data)
     d.append(i)
 print(d[d.index(0) + 1])
