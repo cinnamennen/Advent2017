be8f32ca432b97dddcfbd6d732e9260d523bf679
Caleb Mennen <cmennen@cisco.com>
2018-12-14 14:05:16 -0500

    Fast 17?

--- 17/a.py
+++ 17/a.py
@@ -1,24 +1,31 @@
 import pprint
 import re
+
+from collections import deque
 from tqdm import trange
 
 pp = pprint.PrettyPrinter(indent=4)
 
 
 def process_input():
-    file = 'x.txt'
+    file = 'a.txt'
     data = [_.strip() for _ in open(file).readlines()]
     return data
 
 
 def get_input():
-    data = process_input()
+    data = int(process_input()[0])
 
     return data
 
 def main():
     data = get_input()
-    print(data)
+
+    d = deque([0])
+    for i in range(1,2018):
+        d.rotate(-data)
+        d.append(i)
+    pp.pprint(d[0])
 
 
 if __name__ == '__main__':
---
+++ 17/b.py
@@ -0,0 +1,16 @@
+import pprint
+import re
+
+from collections import deque
+from tqdm import trange
+
+pp = pprint.PrettyPrinter(indent=4)
+
+
+data = 343
+
+d = deque([0])
+for i in trange(1,50000001, mininterval=1, miniters=100):
+    d.rotate(-data)
+    d.append(i)
+print(d[d.index(0) + 1])
