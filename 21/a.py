863a5b443f0c989a061d4c1d4691e5dd6c899f73
Caleb Mennen <cmennen@cisco.com>
2018-12-14 12:39:30 -0500

    re-init

--- 21/a.py
+++ 21/a.py
@@ -1,6 +1,32 @@
 import pprint
+import re
+from tqdm import trange
 
 pp = pprint.PrettyPrinter(indent=4)
 
-file = 'x.txt'
-data = [_.strip() for _ in open(file).readlines()]
+
+def process_input():
+    file = 'x.txt'
+    data = [_.strip() for _ in open(file).readlines()]
+    return data
+
+
+def get_input():
+    data = process_input()
+
+    return data
+
+
+def get_binary(a):
+    a_ = bin(a)[2:]
+    # z = a_.zfill(16)
+    return a_[-16:]
+
+
+def main():
+    data = get_input()
+    print(data)
+
+
+if __name__ == '__main__':
+    main()
